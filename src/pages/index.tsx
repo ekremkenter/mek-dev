0bb74cdb3ddba96b9cab0664bd48578fb0eebafb
Mustafa Ekrem KENTER <ekremkenter@gmail.com>
2023-02-21 14:41:00 +0300

    Add help modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,6 +63,11 @@ const HomePage: NextPage = () => {
           />
         </div>
       </main>
+      <script
+        type="text/javascript"
+        src="https://cdn.popupsmart.com/ps-earthquake/bundle.js"
+        data='{"position":"center-center","devicesTypes":{"desktop":true,"mobile":true},"language":"en","showAfter":10,"colors":{"backgroundColor":"#ffffff","textColor":"#000000","buttonColor":"#000000","buttonTextColor":"#ffffff"},"donations":["afad","wfp"]}'
+      />
     </div>
   );
 };
