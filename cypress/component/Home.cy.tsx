3054e00c11e04ca85c16a66e9c13ef71cd7c8bec
Mustafa Ekrem KENTER <ekremkenter@gmail.com>
2023-12-08 15:24:19 +0300

    Website v2 (#1)

--- cypress/component/Home.cy.tsx
+++
@@ -1,9 +0,0 @@
-import { mount } from 'cypress/react';
-
-import HomePage from '@/pages';
-
-describe('Home.cy.ts', () => {
-  it('playground', () => {
-    mount(<HomePage />);
-  });
-});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,43 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 import Script from 'next/script';
 
+const inter = Inter({ subsets: ['latin'] });
+
+const title = 'Mustafa Ekrem KENTER';
+const description =
+  "Software Craftsman - Mustafa Ekrem KENTER's personal web site";
 const trackingID = 'UA-37973458-7';
 
-function Document() {
-  const title = 'Mustafa Ekrem KENTER';
-  const description =
-    "Software Craftsman - Mustafa Ekrem KENTER's personal web site";
+export const metadata: Metadata = {
+  title,
+  description,
+  metadataBase: new URL('https://mek.dev'),
+  twitter: {
+    site: '@ekremkenter',
+    card: 'summary',
+    creator: '@ekremkenter',
+    title,
+    description,
+  },
+  openGraph: {
+    type: 'website',
+    title,
+    description,
+    locale: 'en_US',
+  },
+};
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <Html>
-      <Head>
+    <html lang="en">
+      <head>
         <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta name="og:type" content="website" />
-        <meta name="author" content="@ekremkenter" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:creator" content="@ekremkenter" />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
         <link rel="icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
@@ -38,17 +57,11 @@ function Document() {
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff"></meta>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
+      </head>
+      <body
+        className={`${inter.className} antialiased text-slate-500 dark:text-slate-400 bg-white dark:bg-slate-900`}
+      >
+        <div className="container mx-auto">{children}</div>
         <Script id="google-analytics" strategy="afterInteractive">
           {`
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -61,8 +74,6 @@ function Document() {
         `}
         </Script>
       </body>
-    </Html>
+    </html>
   );
 }
-
-export default Document;
