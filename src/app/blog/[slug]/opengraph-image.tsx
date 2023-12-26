4cf25923d63859ae14f73ee221a92b3fadf92a4c
Mustafa Ekrem KENTER <ekremkenter@gmail.com>
2023-12-26 12:25:53 +0300

    opengraph-image for blog posts

---
+++ src/app/blog/[slug]/opengraph-image.tsx
@@ -0,0 +1,85 @@
+import { ImageResponse } from "next/og";
+import { allPosts } from "contentlayer/generated";
+import { redirect } from "next/navigation";
+import config from "@/util/config";
+
+export const size = {
+  width: 1200,
+  height: 630,
+};
+export default function Image({ params }: { params: { slug: string } }) {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+  if (!post) redirect("/blog");
+  const { title, description } = post;
+  const fullUrl = `${config.url}/blog/${post._raw.flattenedPath}`;
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background:
+            "rgb(23,23,23) linear-gradient(180deg, rgba(23,23,23,1) 0%, rgba(30,27,75,1) 100%)",
+          width: "100%",
+          height: "100%",
+          color: "white",
+          fontSize: "1.5rem",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {post.heroImage ? (
+          <img
+            alt={`Background`}
+            src={`${config.url}/${post.heroImage}`}
+            style={{
+              position: "absolute",
+              top: 0,
+              right: 0,
+              bottom: 0,
+              left: 0,
+            }}
+          />
+        ) : null}
+        <div
+          style={{
+            position: "absolute",
+            right: "1rem",
+            background: "rgba(0,0,0,0.7)",
+            padding: "2rem",
+            maxWidth: "90%",
+            borderRadius: "1rem",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-end",
+            justifyContent: "flex-end",
+            textAlign: "right",
+          }}
+        >
+          <div
+            style={{
+              fontSize: "3rem",
+              paddingBottom: "1rem",
+            }}
+          >
+            {title}
+          </div>
+          <div>{description}</div>
+          <div
+            style={{
+              paddingTop: "1rem",
+              fontSize: "0.8rem",
+              color: "rgba(255,255,255,0.7)",
+            }}
+          >
+            {fullUrl}
+          </div>
+        </div>
+      </div>
+    ),
+    {
+      ...size,
+    },
+  );
+}
--- src/util/config.ts
+++ src/util/config.ts
@@ -1,7 +1,7 @@
 const config = {
   title: "Mustafa Ekrem KENTER",
   description: "Software Craftsman - Mustafa Ekrem KENTER's personal web site",
-  url: "https://mek.dev",
+  url: process.env.NEXT_PUBLIC_WEBSITE_URL || "https://mek.dev",
   talkUrl: "https://calendar.app.google/ovxBQvBqU3swrf337",
 };
 export default config;
