0b0308c5bf2a074c8027cd2cbeed16391b3d580d
Mustafa Ekrem KENTER <ekremkenter@gmail.com>
2024-04-29 11:25:40 +0300

    Add og title

---
+++ src/app/kilo-hesaplama/opengraph-image.tsx
@@ -0,0 +1,67 @@
+import { ImageResponse } from "next/og";
+
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export function generateImageMetadata() {
+  return [
+    {
+      id: "og",
+      alt: "Kilo Hesaplama",
+      size,
+      contentType: "image/png",
+    },
+  ];
+}
+
+export default function Image() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background:
+            "rgb(23,23,23) linear-gradient(180deg, rgba(23,23,23,1) 0%, rgba(30,27,75,1) 100%)",
+          width: "100%",
+          height: "100%",
+          color: "white",
+          fontSize: "2rem",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            right: "1rem",
+            background: "rgba(0,0,0,0.7)",
+            padding: "2rem",
+            maxWidth: "90%",
+            borderRadius: "1rem",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-end",
+            justifyContent: "flex-end",
+            textAlign: "right",
+          }}
+        >
+          <div
+            style={{
+              fontSize: "4rem",
+              paddingBottom: "2rem",
+            }}
+          >
+            Kilo Hesaplama
+          </div>
+          <div>İdeal kilonuzu hesaplamak için aşağıya boyunuzu yazınız.</div>
+        </div>
+      </div>
+    ),
+    {
+      ...size,
+    },
+  );
+}
--- src/app/kilo-hesaplama/page.tsx
+++ src/app/kilo-hesaplama/page.tsx
@@ -1,78 +1,11 @@
-"use client";
+import { Calculator } from "./_components/calculator";
+import { Metadata } from "next";
 
-import { Input } from "@/components/ui/input";
-import { useEffect, useState } from "react";
+export const metadata: Metadata = {
+  title: "Kilo Hesaplama Aracı",
+  description: "",
+};
 
 export default function CalculatePage() {
-  const [height, setHeight] = useState(180);
-  const [values, setValues] = useState({
-    problem: 0,
-    lowCons: 0,
-    min: 0,
-    max: 0,
-    physical: 0,
-  });
-
-  const calculate = (height: number) => {
-    const min = (height / 2) * 0.8;
-    const max = min * 1.1;
-    const physical = max * 1.1;
-    const lowCons = min * 0.9;
-    const problem = lowCons * 0.9;
-    setValues({
-      problem,
-      lowCons,
-      min,
-      max,
-      physical,
-    });
-  };
-
-  useEffect(() => {
-    calculate(height);
-  }, [height]);
-
-  return (
-    <div className="block">
-      <div className="py-12 sm:py-32 w-full prose dark:prose-invert">
-        <h1>Kilo Hesaplama</h1>
-        <p>İdeal kilonuzu hesaplamak için aşağıya boyunuzu yazınız.</p>
-        <Input
-          placeholder="Boyunuz"
-          value={height}
-          type="number"
-          onChange={(e) => {
-            const height = Number.parseInt(e.target.value);
-
-            if (Number.isInteger(height)) {
-              setHeight(height);
-            }
-          }}
-        />
-
-        <div className="mt-2 grid grid-cols-1 gap-1 sm:grid-cols-2 sm:gap-3">
-          <Card
-            title="Aşağısında gözle görülür sorunlar, saçma konuşmalar"
-            value={values.problem}
-          />
-          <Card
-            title="Aşağısında bilinç eski performansta değil"
-            value={values.lowCons}
-          />
-          <Card title="İdeal kiloda dip kısım" value={values.min} />
-          <Card title="Üst Sınır" value={values.max} />
-          <Card title="Üstünde gözle görülür sıkıntı" value={values.physical} />
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function Card({ title, value }: { title: string; value: number }) {
-  return (
-    <div className={"flex flex-col items-start justify-start"}>
-      <h3 className="my-2">{value.toFixed(1)}</h3>
-      <p>{title}</p>
-    </div>
-  );
+  return <Calculator />;
 }
