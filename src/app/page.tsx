20692b19b4e60fe4dcac69ed1265301f7705e366
Mustafa Ekrem KENTER <ekremkenter@gmail.com>
2024-08-27 14:04:49 +0300

    Update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import GithubIcon from "@/components/icons/github-icon";
 import LinkedinIcon from "@/components/icons/linkedin-icon";
 import TwitterIcon from "@/components/icons/twitter-icon";
 import MediumIcon from "@/components/icons/medium-icon";
-import UpworkIcon from "@/components/icons/upwork-icon";
 import config from "@/util/config";
 
 export default function Home() {
@@ -13,32 +12,36 @@ export default function Home() {
       <Avatar />
       <h1 className="text-4xl font-bold mt-8">Mustafa Ekrem KENTER</h1>
       <h2 className="text-2xl font-semibold italic mt-2">
-        Software craftsman, consultant, and father of two.
+        Software craftsman, and father of two.
       </h2>
       <p className="mt-4">
         I&apos;m Ekrem, a passionate software engineer and entrepreneur based in
         Istanbul, Turkiye. With over 15 years of experience in full-stack web
         and mobile development, chatbot creation, and serverless architecture, I
         thrive on embracing the latest technologies and industry best practices.
-        Family is at the core of my inspiration, and I am grateful for the
-        unwavering support of my amazing wife.
       </p>
       <p className="mt-4">
-        As a dedicated software development consultant, I channel this support
-        into my work, crafting innovative solutions that address real-world
-        challenges and deliver meaningful value to customers and stakeholders.
+        Family is at the core of my inspiration, and I am grateful for the
+        unwavering support of my amazing wife. I channel this support into my
+        work, crafting innovative solutions that address real-world challenges
+        and deliver meaningful value to customers and stakeholders.
+      </p>
+      <p className="mt-4">
         My mission is to guide businesses in achieving their technological
         objectives, fostering growth, and staying ahead in today&apos;s dynamic
-        digital landscape.
-      </p>
-      <div className="mt-6 flex items-center justify-center gap-x-6 lg:justify-start">
+        digital landscape. Currently working for{" "}
         <a
-          href={config.talkUrl}
-          className="font-semibold hover:transform hover:scale-105 transition duration-150 ease-in-out"
+          href="https://www.linkedin.com/company/turkishtechnology"
           target="_blank"
         >
-          Need help with your project? Let&apos;s Talk{" "}
-          <span aria-hidden="true">→</span>
+          Turkish Technology
+        </a>
+        , a subsidiary of Turkish Airlines, as{" "}
+        <span className="font-semibold">Digital Lab Team Lead.</span>
+      </p>
+      <div className="mt-6 flex items-center justify-center gap-x-6 lg:justify-start">
+        <a href={config.talkUrl} target="_blank">
+          Let&apos;s Talk <span aria-hidden="true">→</span>
         </a>
       </div>
       <div className="mt-6 flex gap-6">
@@ -81,14 +84,6 @@ export default function Home() {
           target="_blank"
         >
           <MediumIcon />
-        </a>
-        <a
-          className="group -m-1 p-1"
-          aria-label="Hire on Upwork"
-          href="https://www.upwork.com/freelancers/~012acf61dac0452080"
-          target="_blank"
-        >
-          <UpworkIcon />
         </a>
       </div>
     </div>
--- src/util/config.ts
+++ src/util/config.ts
@@ -2,6 +2,6 @@ const config = {
   title: "Mustafa Ekrem KENTER",
   description: "Software Craftsman - Mustafa Ekrem KENTER's personal web site",
   url: process.env.NEXT_PUBLIC_WEBSITE_URL || "https://mek.dev",
-  talkUrl: "https://calendar.app.google/ovxBQvBqU3swrf337",
+  talkUrl: "https://cal.com/ekremkenter/15min",
 };
 export default config;
